377659170ccdd854cf5fc53e76fa38c1fe711d6b
gtmnayan <gtmnayan@gmail.com>
2023-01-25 10:35:09 +0545

    simplify repro

--- src/entry-server.js
+++ src/entry-server.js
@@ -1,16 +1,10 @@
-import App from './App.svelte'
 import foo from "./test.js";
 
 export function render() {
 debugger;
   const abc = foo();
 
-  try {
-    0;
-  } catch {
+  {
     const foo = 23;
   }
-
-  // @ts-ignore
-  return App.render()
 }
